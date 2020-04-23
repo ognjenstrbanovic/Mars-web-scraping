f99a0c0ef339ffb58f4afa7f56991e50bd6cca1b
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-04-23 14:56:45 -0400

    additional commit

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,8 +1,59 @@
-# import necessary libraries
-from flask import Flask, render_template
+from splinter import Browser
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
-# create instance of Flask app
-app = Flask(__name__)
+url_1 = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+response_1 = requests.get(url_1)
+soup_1 = BeautifulSoup(response_1.text, "html.parser")
+news_title = soup_1.find(class_ = "content_title").text.strip()
+news_p = soup_1.find(class_ = "rollover_description_inner").text.strip()
 
 
-# create route that renders index.html template
+
+url_3 = "https://twitter.com/marswxreport?lang=en"
+response_2 = requests.get(url_3)
+soup_3 = BeautifulSoup(response_2.text, "html.parser")
+mars_weather = \
+soup_3.find("p", class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").\
+text.strip(" hPapic.twitter.com/SkjAdWsePB")
+
+url_4 = "https://space-facts.com/mars/"
+tables = pd.read_html(url_4)
+df = tables[0]
+df.columns = ["Mars Facts", "Data"]
+df = df.replace("\n", "", regex=True)
+html_table = df.to_html()
+
+
+url_a = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
+url_b = "https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced"
+url_c = "https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced"
+url_d = "https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced"
+response_a = requests.get(url_a)
+soup_a = BeautifulSoup(response_a.text, "html.parser")
+response_b = requests.get(url_b)
+soup_b = BeautifulSoup(response_b.text, "html.parser")
+response_c = requests.get(url_c)
+soup_c = BeautifulSoup(response_c.text, "html.parser")
+response_d = requests.get(url_d)
+soup_d = BeautifulSoup(response_d.text, "html.parser")
+titles = []
+img_urls = []
+for url in url_a, url_b, url_c, url_d:
+    titles.append(soup_a.find("h2", class_ = "title").text)
+    titles.append(soup_b.find("h2", class_ = "title").text)
+    titles.append(soup_c.find("h2", class_ = "title").text)
+    titles.append(soup_d.find("h2", class_ = "title").text)
+    img_urls.append(soup_a.find("li").a["href"])
+    img_urls.append(soup_b.find("li").a["href"])
+    img_urls.append(soup_c.find("li").a["href"])
+    img_urls.append(soup_d.find("li").a["href"])
+    break
+hemisphere_image_urls = dict(zip(titles, img_urls))
+
+def scrape():
+    scraped_data = dict(news_title = soup_1.find(class_ = "content_title").text.strip(), news_p = soup_1.find(class_ = "rollover_description_inner").text.strip(), mars_weather = soup_3.find("p", class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text.strip(" hPapic.twitter.com/SkjAdWsePB"), html_table = df.to_html(), hemisphere_image_urls = dict(zip(titles, img_urls)))
+    return scraped_data
+
+scrape()
