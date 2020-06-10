5293d3377abbd1bc71fd269e0e5b39547b675a85
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-06-10 11:43:55 -0400

    separating tasks w/ comments

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -6,12 +6,14 @@ import pandas as pd
 import time
 
 # using BeautifulSoup
+# Task 1
 url_1 = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 response_1 = requests.get(url_1)
 soup_1 = BeautifulSoup(response_1.text, "html.parser")
 news_title = soup_1.find(class_ = "content_title").text.strip()
 news_p = soup_1.find(class_ = "rollover_description_inner").text.strip()
 
+# Task 2
 executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 browser = Browser("chrome", **executable_path, headless=False)
 url_2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
@@ -21,7 +23,7 @@ time.sleep(1)
 soup_2 = BeautifulSoup(html, "html.parser")
 result = soup_2.find("img", class_ = "thumb").get("src")
 featured_image_url = f"https://www.jpl.nasa.gov{result}"
-
+# Task 3
 url_3 = "https://twitter.com/marswxreport?lang=en"
 response_2 = requests.get(url_3)
 soup_3 = BeautifulSoup(response_2.text, "html.parser")
@@ -29,6 +31,7 @@ mars_weather = \
 soup_3.find("p", class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").\
 text.strip(" hPapic.twitter.com/SkjAdWsePB")
 
+# Task 4
 url_4 = "https://space-facts.com/mars/"
 tables = pd.read_html(url_4)
 df = tables[0]
@@ -36,6 +39,7 @@ df.columns = ["Mars Facts", "Data"]
 df = df.replace("\n", "", regex=True)
 html_table = df.to_html()
 
+# Task 5
 url_a = "https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced"
 url_b = "https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced"
 url_c = "https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced"
@@ -65,5 +69,4 @@ hemisphere_image_urls = dict(zip(titles, img_urls))
 def scrape():
     scraped_data = dict(news_title = soup_1.find(class_ = "content_title").text.strip(), news_p = soup_1.find(class_ = "rollover_description_inner").text.strip(), featured_image_url = f"https://www.jpl.nasa.gov{result}", mars_weather = soup_3.find("p", class_ = "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text.strip(" hPapic.twitter.com/SkjAdWsePB"), html_table = df.to_html(), hemisphere_image_urls = dict(zip(titles, img_urls)))
     return scraped_data
-
 print(scrape())
